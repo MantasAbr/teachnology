75456dfcb8f010ec082c89f648045d53ce76f9c9
Mantas <mantasabra@gmail.com>
2021-10-12 19:31:42 +0300

    Added a primitive circular menu. Will improve later

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,20 +17,23 @@
     <body class="antialiased">
             <div>
                 <h1 class="splash">TEACHNOLOGY</h1>
-                    <a class="title" href="{{ url('logIn') }}">Prisijungti</a>
+                    <!--<a class="title" href="{{ url('logIn') }}">Prisijungti</a>
                     <a class="title" href="{{ url('profile') }}">Profilis</a>
                     <a class="title" href="{{ url('testsList') }}">Testų sąrašas</a>
                     <a class="title" href="{{ url('myTestsList') }}">Mano testų sąrašas</a>
                     <a class="title" href="{{ url('statistics') }}">Statistika</a>
-                    <a class="title" href="{{ url('currency') }}">Pirkti valiutą/Premium</a>
+                    <a class="title" href="{{ url('currency') }}">Pirkti valiutą/Premium</a>-->
             </div>
-            
-            <!--<div class="circle">
-                <div class="tick"></div>
-                <div class="tick"></div>
-                <div class="tick"></div>
-                <div class="tick"></div>
-                <div class="tick"></div>
-            </div> -->
+
+            <div class="container" style="--m: 6; --tan: 0.41">
+                <!-- Čia logo galės per vidurį būt <img><img>-->
+                <a  class="title" style="--i: 1" href="{{ url('profile') }}">Profilis</a>
+                <a  class="title" style="--i: 2" href="{{ url('testsList') }}">Testų sąrašas</a>
+                <a  class="title" style="--i: 3" href="{{ url('myTestsList') }}">Mano testų sąrašas</a>
+                <a  class="title" style="--i: 4" href="{{ url('statistics') }}">Statistika</a>
+                <a  class="title" style="--i: 5" href="{{ url('currency') }}">Pirkti valiutą/Premium</a>
+                <a  class="title" style="--i: 6" href="{{ url('currency') }}">Pirkti valiutą/Premium</a>
+            </div>
+
     </body>
 </html>

8c523d2943020ce545302e2c00efc0bc51b3c314
Rūta <ruta.galinkevice@gmail.com>
2021-12-02 16:07:54 +0200

    Breakpoints

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -93,20 +93,6 @@
                                 </div>
                             @endif
                         </div>
-                      @if(Auth::user()->role == 1)
-                        @if($usersProfile->is_blocked == 1)
-                            <a href="{{ route('status', ['id' => $usersProfile->id, 'status_code'=>0]) }}" class="btn">
-                        <button>Blokuoti</button>
-                            </a>
-                            @else
-                            <a href="{{ route('status', ['id' => $usersProfile->id, 'status_code'=>1]) }}" class="btn">
-                        <button>Atblokuoti</button>
-                            </a>
-                        
-                            @endif
-                            @endif
-
-                            
                     </div>
         </div>
 </div>
